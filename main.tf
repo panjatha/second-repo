7215b1e4a2faeba309746838b475c61857ddb771
panjatha <panjatha.mandeep@gmail.com>
2025-05-09 11:09:52 +0530

    storage account code added

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ resource "azurerm_resource_group" "name" {
     location = "East US"
   
 }
+
+resource "azurerm_storage_account" "storage" {
+    name = "devstorageprod01"
+    resource_group_name = azurerm_resource_group.name.name
+    location = azurerm_resource_group.name.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+
+  
+}
